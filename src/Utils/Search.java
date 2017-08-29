3b7e576f02ee8728cf54a199bf93a767636e420c
liuxl <luxurymask@163.com>
2017-08-29 22:04:24 +0800

    StrongBuffer美团面试
    有序序列，含重复值，查找k，如果k有多个返回最大index。

--- src/Utils/Search.java
+++ src/Utils/Search.java
@@ -10,8 +10,26 @@ public class Search {
 		return -2;
 	}
 	
+	/**
+	 * 有重复，返回最大index。
+	 * @param A
+	 * @param k
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public static int findTheLastIndex(int[] A, int k, int left, int right){
+		if(left > right) return -1;
+		int m = left + (right - left) / 2;
+		if(A[m] == k && (m == A.length - 1 || A[m + 1] != A[m])) return m;
+		if(A[m] > k) return findTheLastIndex(A, k, left, m - 1);
+		return findTheLastIndex(A, k, m + 1, right);
+	}
+	
 	public static void main(String[] args){
 		int[] array = {1, 2, 3, 4, 5, 6};
 		System.out.println(binarySearch(array, 0, 0, 5));
+		int[] array2 = {1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 6};
+		System.out.println(findTheLastIndex(array2, 1, 0, 10));
 	}
 }
