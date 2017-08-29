e1c809936a60b7d691ba4809c2f980d7e12b2909
liuxl <luxurymask@163.com>
2017-08-29 22:30:53 +0800

    binary search non-recursively

--- src/Utils/Search.java
+++ src/Utils/Search.java
@@ -10,6 +10,16 @@ public class Search {
 		return -2;
 	}
 	
+	public static int binarySearchNonRecursively(int[] A, int k, int left, int right){
+		while(left <= right){
+			int m = left + (right - left) / 2;
+			if(A[m] == k) return m;
+			if(A[m] < k) left = m + 1;
+			if(A[m] > k) right = m - 1;
+		}
+		return -1;
+	}
+	
 	/**
 	 * 有重复，返回最大index。
 	 * @param A
@@ -26,10 +36,25 @@ public class Search {
 		return findTheLastIndex(A, k, m + 1, right);
 	}
 	
+	public static int findTheLastIndexNonRecursively(int[] A, int k, int left, int right){
+		while(left <= right){
+			int m = left + (right - left) / 2;
+			if(A[m] == k && (m == A.length - 1 || A[m + 1] != A[m])) return m;
+			if(A[m] > k){
+				right = m - 1;
+			}else{
+				left = m + 1;
+			}
+		}
+		return -1;
+	}
+	
 	public static void main(String[] args){
 		int[] array = {1, 2, 3, 4, 5, 6};
 		System.out.println(binarySearch(array, 0, 0, 5));
+		System.out.println(binarySearch(array, 5, 0, 5));
 		int[] array2 = {1, 1, 2, 3, 3, 3, 4, 5, 6, 6, 6};
 		System.out.println(findTheLastIndex(array2, 1, 0, 10));
+		System.out.println(findTheLastIndexNonRecursively(array2, 3, 0, 10));
 	}
 }
